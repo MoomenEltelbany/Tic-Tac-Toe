2148bef48b6d0505aa42a83096109b44ef53ef58
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2024-01-15 21:27:29 -0300

    Update Player.jsx
    Dynamic Input Names

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 
 export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
+    const [inputValue, setInputValue] = useState("");
 
     function handleEditClick() {
         setIsEditing(!isEditing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    let playerName = (
+        <span className="player-name">{inputValue || "Player Name"}</span>
+    );
 
     if (isEditing) {
-        playerName = <input type="text" required />;
+        playerName = (
+            <input
+                type="text"
+                required
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+        );
     }
 
     return (
